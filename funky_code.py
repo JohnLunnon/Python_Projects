8a017ba5fe9c076465b929d4ad7c19a9fe35042a
JohnLunnon <57829786+JohnLunnon@users.noreply.github.com>
2024-04-22 15:50:38 +0100

    Add files via upload

---
+++ funky_code.py
@@ -0,0 +1,39 @@
+from ensurepip import version
+
+
+char = "#"
+
+# Print the letter 'C'
+print(" {0}".format(char*4))
+print("{0} ".format(char))
+print("{0} ".format(char))
+print("{0} ".format(char))
+print(" {0}".format(char*4))
+
+# Add an empty line for spacing
+print("")
+
+# Print the letter 'O'
+print(" {0}".format(char*3))
+print("{0}   {0}".format(char))
+print("{0}   {0}".format(char))
+print(" {0}".format(char*3))
+
+# Add an empty line for spacing
+print("")
+
+# Print the letter 'D'
+print("{0} ".format(char*4))
+print("{0}   {0}".format(char))
+print("{0}   {0}".format(char))
+print("{0} ".format(char*4))
+
+# Print an empty line for spacing
+print("")
+
+# Print the letter 'E'
+print("{0}  ".format(char*4))
+print("{0}".format(char))
+print("{0}  ".format(char*4))
+print("{0}".format(char))
+print("{0}  ".format(char*4))
