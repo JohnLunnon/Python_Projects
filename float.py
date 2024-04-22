dc52c9441eb4041362f5cc5d24b7ab9e0a9bea9c
JohnLunnon <57829786+JohnLunnon@users.noreply.github.com>
2024-04-22 17:22:10 +0100

    Add files via upload

---
+++ float.py
@@ -0,0 +1,18 @@
+
+# x = 12 / 4
+
+# y = 5 + 6.1
+
+# print("value of x is {x}")
+# print("type of x is {type(x)}")
+# print("value of y is {y}")
+# print("type of y is {type(y)}")
+
+x = 5
+y = 2.5
+z = x * y
+
+print(type(z))
+git push
+
+git commit
